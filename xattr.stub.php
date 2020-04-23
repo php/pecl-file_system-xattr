33cb19461f26a0bf0881f9578a481934b53e3aee
Remi Collet <remi@php.net>
2020-04-23 12:44:57 +0200

    get and list may return false

--- xattr.stub.php
+++ xattr.stub.php
@@ -2,11 +2,19 @@
 
 function xattr_set(string $path, string $name, string $value, int $flag = 0): bool {}
 
+#if PHP_MAJOR_VERSION < 8
 function xattr_get(string $path, string $name, int $flags = 0): string {}
+#else
+function xattr_get(string $path, string $name, int $flags = 0): string|false {}
+#endif
 
 function xattr_supported(string $path, int $flags = 0): bool {}
 
 function xattr_remove(string $path, string $name, int $flags = 0): bool {}
 
+#if PHP_MAJOR_VERSION < 8
 function xattr_list(string $path, int $flags = 0): array {}
+#else
+function xattr_list(string $path, int $flags = 0): array|false {}
+#endif
 
